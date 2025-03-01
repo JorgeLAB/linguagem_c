96ac6593371c1db7227d63b69c57d3e7c21fed7a
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-01 11:37:19 -0300

    guessing game - add comments

--- guessing_game/adivinhacao.c
+++ guessing_game/adivinhacao.c
@@ -2,13 +2,18 @@
 
 int main() {
 
-  // imprime o cabeçalho do jogo
+  // prints the game header
   printf("\t**************************************\n");
   printf("\tBem vindo ao nosso jogo de adivinhação *\n");
   printf("\t**************************************\n");
 
+  /*
+    Variables must have single scope. Don`t have 2 declarations of this variable numerosecreto.
+    If you do this by mistake, the compilator went issue a warning and will don`t generate the executable program.
+  */
+
   int numerosecreto = 42;
 
-  // interpolação com o inteiro
+  // interpolation with the integer
   printf("O número %d é o secreto. Não conte para ninguém!\n", numerosecreto);
 }
