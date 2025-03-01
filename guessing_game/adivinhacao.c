0c18e4c6d8bcac7793aa456b243166d125408036
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-01 11:20:49 -0300

    guessing game - interpolation and variable declaration

--- guessing_game/adivinhacao.c
+++ guessing_game/adivinhacao.c
@@ -6,4 +6,9 @@ int main() {
   printf("\t**************************************\n");
   printf("\tBem vindo ao nosso jogo de adivinhação *\n");
   printf("\t**************************************\n");
+
+  int numerosecreto = 42;
+
+  // interpolação com o inteiro
+  printf("O número %d é o secreto. Não conte para ninguém!\n", numerosecreto);
 }
