f10a6f1aac41ef4e8910960cfe55593946c37929
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-01 11:12:50 -0300

    init - guessing game

---
+++ guessing_game/adivinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+  // imprime o cabeçalho do jogo
+  printf("\t**************************************\n");
+  printf("\tBem vindo ao nosso jogo de adivinhação *\n");
+  printf("\t**************************************\n");
+}
