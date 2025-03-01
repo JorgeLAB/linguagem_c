a583f92b4098f9dafbc21761917f61f9ad66fd5f
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-01 13:59:21 -0300

    guessing game - else if conditional

--- 1_guessing_game/adivinhacao.c
+++ 1_guessing_game/adivinhacao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 int main() {
 
   // prints the game header
@@ -18,13 +20,15 @@ int main() {
 
   // repetition structure
 
-  for(int i = 1; i <= 3; i++) {
-    printf("Tentativa %d de 3\n", i);
+  for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+    printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
 
     printf("Qual é o seu chute? ");
     scanf("%d", &chute);
 
     int acertou = ( chute == numerosecreto );
+    int maior = chute > numerosecreto;
+    int menor = chute < numerosecreto;
 
     if(acertou) {
       printf("Você acertou! O número secreto é %d.\n", chute);
@@ -32,17 +36,15 @@ int main() {
 
       break;
     }
-    else {
-      int maior = chute > numerosecreto;
 
-      if(maior){
-        printf("Seu chute foi maior que o número secreto\n");
-      }
-      else {
-        printf("Seu chute foi menor que o número secreto\n");
-      }
+    else if(maior) {
+      printf("Seu chute foi maior que o número secreto\n");
+    }
+
+    else {
+      printf("Seu chute foi menor que o número secreto\n");
     }
   }
 
-  printf("Fim de Jogo!");
+  printf("Fim de Jogo!\n");
 }
