7b2dcb6d303835445bd9f359d6e456823a6a6d69
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-05 00:55:31 -0300

    hangman game - created function desenha forca

--- 2_hangman_game/hangman_game.c
+++ 2_hangman_game/hangman_game.c
@@ -31,6 +31,21 @@ int jachutou(char letra, char chutes[26], int tentativas) {
   return achou;
 }
 
+void desenhaforca(char palavrasecreta[20], char chutes[26], int tentativas) {
+	for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+	  int achou = jachutou( palavrasecreta[i], chutes, tentativas);
+
+	  if(achou) {
+	    printf("%c ", palavrasecreta[i]);
+	  } else {
+	    printf("_ ");
+	  }
+	}
+
+	printf("\n");
+}
+
 int main() {
   char palavrasecreta[20];
   sprintf(palavrasecreta, "MELANCIA");
@@ -44,18 +59,8 @@ int main() {
   abertura();
 
   do {
-    for(int i = 0; i < strlen(palavrasecreta); i++) {
+    desenhaforca(palavrasecreta, chutes, tentativas);
 
-      int achou = jachutou( palavrasecreta[i], chutes, tentativas);
-
-      if(achou) {
-        printf("%c ", palavrasecreta[i]);
-      } else {
-        printf("_ ");
-      }
-    }
-
-	  printf("\n");
 	  chuta(chutes, &tentativas);
   } while (!acertou && !enforcou);
 }
