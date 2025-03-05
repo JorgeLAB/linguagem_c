879c500a722e20ba6959391dc467c59259e703ea
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-05 00:37:18 -0300

    hangman game - create function with return

--- 2_hangman_game/hangman_game.c
+++ 2_hangman_game/hangman_game.c
@@ -18,6 +18,19 @@ void chuta(char chutes[26], int* tentativas) {
   (*tentativas)++;
 }
 
+int jachutou(char letra, char chutes[26], int tentativas) {
+  int achou = 0;
+
+  for(int j = 0; j < tentativas; j++) {
+    if(chutes[j] == letra) {
+      achou = 1;
+      break;
+    }
+  }
+
+  return achou;
+}
+
 int main() {
   char palavrasecreta[20];
   sprintf(palavrasecreta, "MELANCIA");
@@ -32,14 +45,8 @@ int main() {
 
   do {
     for(int i = 0; i < strlen(palavrasecreta); i++) {
-      int achou = 0;
 
-      for(int j = 0; j < tentativas; j++) {
-        if(chutes[j] == palavrasecreta[i]) {
-          achou = 1;
-          break;
-        }
-      }
+      int achou = jachutou( palavrasecreta[i], chutes, tentativas);
 
       if(achou) {
         printf("%c ", palavrasecreta[i]);
@@ -47,6 +54,7 @@ int main() {
         printf("_ ");
       }
     }
+
 	  printf("\n");
 	  chuta(chutes, &tentativas);
   } while (!acertou && !enforcou);
