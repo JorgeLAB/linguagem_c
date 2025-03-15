55e13ec9602fe337e380e128d0d7a27d7cfb02fc
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-14 23:26:54 -0300

    pacman - struct for location hero

--- 3_pacman_game/mapa.h
+++ 3_pacman_game/mapa.h
@@ -6,7 +6,15 @@ struct mapa {
 
 typedef struct mapa MAPA;
 
+struct posicao {
+  int x;
+  int y;
+};
+
+typedef struct posicao POSICAO;
+
 void liberamapa(MAPA* m);
 void alocamapa(MAPA* m);
 void lermapa(MAPA* m);
 void imprimimapa(MAPA* m);
+void encontramapa(MAPA* m, POSICAO* p, char c);
