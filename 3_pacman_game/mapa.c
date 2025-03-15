aebd5b7b8ef355cb3c666ba9f417173f3af7fceb
Jorge Borges <jorgetrabalho2016@gmail.com>
2025-03-15 11:58:53 -0300

    pacman - create random ghost moviments

--- 3_pacman_game/mapa.c
+++ 3_pacman_game/mapa.c
@@ -36,6 +36,11 @@ int ehvazia(MAPA* m, int x, int y) {
          m->matriz[x][y] == MOEDA;
 }
 
+int podeandar(MAPA* m, int x, int y) {
+	return ehvazia(m, x, y) &&
+	       ehvalida(m, x, y);
+}
+
 void encontramapa(MAPA* m, POSICAO* p, char c) {
 	for(int i = 0; i < m->linhas; i++) {
     for(int j = 0; j < m->colunas; j++) {
